81dec66134d470eef12a8da6c01bf098c9efc222
jeevakavie-ux <jeevakavie@gmail.com>
2026-09-21 20:30:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const openModal = document.getElementById("openModal");
+const modal = document.getElementById("modal");
+const closeModal = document.querySelector(".close-modal");
+
+openModal.addEventListener("click", function () {
+  modal.style.display = "flex";
+});
+
+closeModal.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+
+modal.addEventListener("click", function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
